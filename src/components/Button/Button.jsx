b9a2ee379c6a0d4c3414bf260126bf3ef22ecc2a
Yuliy Yefymenko <uliy86@gmail.com>
2023-11-21 19:01:37 -0500

    Fix Button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Button = ({ clickHandler }) => {
-  return <button onClick={clickHandler}>Button</button>;
+const Button = ({ clickHandler, text = "Add default value" }) => {
+  return <button onClick={clickHandler}>{text}</button>;
 };
 
 export default Button;
